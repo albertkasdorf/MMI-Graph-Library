57c54f90b3892ab786edb0b9068a8d68232203ad
Albert Kasdorf <albert.kasdorf@live.de>
2018-06-22 18:36:53 +0200

    Unit test infrastructure added.

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <graph_vertex.h>
+#include <graph_comparer.h>
+
+TEST(test_case_name, test_name)
+{
+	EXPECT_TRUE(true);
+}
+
+
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
